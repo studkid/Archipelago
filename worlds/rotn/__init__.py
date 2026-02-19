03fea524af118716c14b8e7eaccf03734ce6a1b6
Emily <eric.poore@gmail.com>
2026-02-18 23:21:27 -0600

    Clean up unused line

--- worlds/rotn/__init__.py
+++ worlds/rotn/__init__.py
@@ -136,7 +136,6 @@ class RotNWorld(World):
         exclude_songs = self.options.exclude_songs.value
 
         self.starting_songs = [s for s in start_items if s in available_song_keys]
-        # self.included_songs = [s for s in include_songs if s in available_song_keys and s not in self.starting_songs]
 
         for song in include_songs:
             if song in available_song_keys and song not in self.starting_songs:
