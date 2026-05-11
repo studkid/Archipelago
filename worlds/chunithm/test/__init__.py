ae6e29565a287360834be11934645f92916cb1cd
studkid <eric.poore@gmail.com>
2026-05-11 10:01:12 -0500

    [All] Remove Tests

--- worlds/chunithm/test/__init__.py
+++
@@ -1,9 +0,0 @@
-from test.bases import WorldTestBase
-from .. import ChuniWorld
-from typing import cast
-
-class RotNTestBase(WorldTestBase):
-    game = "Rift of the Necrodancer"
-
-    def get_world(self) -> ChuniWorld:
-        return cast(ChuniWorld, self.multiworld.worlds[1])
--- worlds/maimaidx/test/__init__.py
+++
@@ -1,9 +0,0 @@
-from test.bases import WorldTestBase
-from .. import MaiWorld
-from typing import cast
-
-class RotNTestBase(WorldTestBase):
-    game = "Rift of the Necrodancer"
-
-    def get_world(self) -> MaiWorld:
-        return cast(MaiWorld, self.multiworld.worlds[1])
