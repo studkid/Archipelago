91f66f8620ccdf96acfbb26707fd36473e2381dc
Emily <eric.poore@gmail.com>
2026-08-20 16:46:36 -0500

    Another warp rewrite?

--- worlds/gstce/GameState.py
+++ worlds/gstce/GameState.py
@@ -4,6 +4,7 @@ import ctypes
 import functools
 import struct
 import CommonClient
+import struct
 
 import psutil
 import pymem.process
@@ -111,7 +112,6 @@ class GameStateManager:
         tp_target_id = 0x25EACD
         tp_location = self.process.read_bytes(self.process.base_address + tp_target_id, 5)
         print(tp_location.hex())
-        setTPTargetMem = self.process.allocate(0xB0)
         singleCall = self.process.allocate(0x4)
         regBackup = self.process.allocate(0x4)
 
@@ -121,30 +121,52 @@ class GameStateManager:
                 # mov dword ptr ["00400000"+4FB074],01
                 # self.process.write_bytes(self.process.base_address + tp_target_id, 0x01, 1)
 
-                values = [
-                    # Change Current Map
-                    b"\xC7\x00\x4D\x61\x70\x5F",     # mov [eax],5F70614D    // Map_
-                    b"\xC7\x40\x04\x0E\x14\xBC\x03", # mov [eax+8],6863756C  // luch
-                    b"\xC7\x40\x08\x6C\x75\x63\x68", # mov [eax+4],62657550  // Pueb
-                    b"\xC7\x40\x0C\x6F\x2E\x6C\x65", # mov [eax+C],656C2E6F  // o.le
-                    b"\xC7\x40\x0A\x76\x65\x6C\x00", # mov [eax+10],006C6576 // vel
-                    b"\xC7\x40\x0E\x00\x00\x00\x00", # mov [eax+14],00000000 //
+                self.process.write_int(singleCall, 0)
 
-                    # Reg Backup
-                    b"\x89\x1D\x00\x00\x00\x00", # mov [regbackup],ebx
-                    # Set player spawn checkpoint location
-                    b"\x8B\x1D\xA4\xE7\x53\x00", # mov ebx,[00400000+5367716]
-                    b"\xC6\x83\x84\x00\x00\x00\x3E", # mov byte ptr [ebx+132],62
-                    # Restore Reg State
-                    b"\x8B\x1D\x00\x00\x00\x00", # mov ebx,[regbackup]
-                    # Set Call
-                    b'\xE8\x2E\xED\xFF\xFF' # call 0065D800 
-                ]
-                for value in values:
-                    self.process.write_bytes(setTPTargetMem, value, len(value))
-                    tp_target_id = setTPTargetMem + (len(value) * 8)
+                # shellcode = b"\x83\x3d" + struct.pack("<I", singleCall) + b"\x00\x00\x00\x00" # cmp [singleCall],00000000
+                # shellcode += b"\x0F\x85" + struct.pack("<I", )
 
-                # self.process.write_bytes(regBackup, )
+                shellcode = b"\xC7\x05\x74\xB0\x51\x00\x01\x00\x00\x00" # mov dword ptr ["00400000"+4FB074],01
+
+                shellcode += b"\xC7\x00\x4D\x61\x70\x5F"     # mov [eax],5F70614D    // Map_
+                shellcode += b"\xC7\x40\x04\x0E\x14\xBC\x03" # mov [eax+8],6863756C  // luch
+                shellcode += b"\xC7\x40\x08\x6C\x75\x63\x68" # mov [eax+4],62657550  // Pueb
+                shellcode += b"\xC7\x40\x0C\x6F\x2E\x6C\x65" # mov [eax+C],656C2E6F  // o.le
+                shellcode += b"\xC7\x40\x0A\x76\x65\x6C\x00" # mov [eax+10],006C6576 // vel
+                shellcode += b"\xC7\x40\x0E\x00\x00\x00\x00" # mov [eax+14],00000000 //
+
+                # shellcode += b"\x8B\x1D\xA4\xE7\x53\x00" # mov ebx,[00400000+5367716]
+                # shellcode += b"\xC6\x83\x84\x00\x00\x00\x3E" # mov byte ptr [ebx+132],62
+
+                shellcode += b'\xE8\x2E\xED\xFF\xFF'
+
+                setTPTargetMem = self.process.allocate(len(shellcode))
+                self.process.write_bytes(setTPTargetMem, shellcode, len(shellcode))
+
+                # values = [
+                #     # Change Current Map
+                #     b"\xC7\x00\x4D\x61\x70\x5F",     # mov [eax],5F70614D    // Map_
+                #     b"\xC7\x40\x04\x0E\x14\xBC\x03", # mov [eax+8],6863756C  // luch
+                #     b"\xC7\x40\x08\x6C\x75\x63\x68", # mov [eax+4],62657550  // Pueb
+                #     b"\xC7\x40\x0C\x6F\x2E\x6C\x65", # mov [eax+C],656C2E6F  // o.le
+                #     b"\xC7\x40\x0A\x76\x65\x6C\x00", # mov [eax+10],006C6576 // vel
+                #     b"\xC7\x40\x0E\x00\x00\x00\x00", # mov [eax+14],00000000 //
+
+                #     # Reg Backup
+                #     b"\x89\x1D\x00\x00\x00\x00", # mov [regbackup],ebx
+                #     # Set player spawn checkpoint location
+                #     b"\x8B\x1D\xA4\xE7\x53\x00", # mov ebx,[00400000+5367716]
+                #     b"\xC6\x83\x84\x00\x00\x00\x3E", # mov byte ptr [ebx+132],62
+                #     # Restore Reg State
+                #     b"\x8B\x1D\x00\x00\x00\x00", # mov ebx,[regbackup]
+                #     # Set Call
+                #     b'\xE8\x2E\xED\xFF\xFF' # call 0065D800 
+                # ]
+                # for value in values:
+                #     self.process.write_bytes(setTPTargetMem, value, len(value))
+                #     tp_target_id = setTPTargetMem + (len(value) * 8)
+
+                # # self.process.write_bytes(regBackup, )
 
                 # Execute
                 pointer = self.resolvePointers(self.process.read_uint(self.process.base_address + 0x51E7A4), [0x132])
